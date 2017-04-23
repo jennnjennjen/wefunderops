9c3f82f837f5654696828bf57d0a464ffc6e6f88
Jennifer Marie Lin <jenn@wefunder.com>
2017-04-22 22:25:44 -0700

    tag

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,15 @@
 class PagesController < ApplicationController
 
+@calctool = 5
+
 def calculate
-  @calctool = Calctool.find params[:fiscalyear]
+
+  validates :month, numericality: true
+  validates :year, numericality: true
+
+  @calctool = params[:fiscalyear][:company]
+  dates = :fiscalyear + 120
+  return dates 
   flash[:notice] = "Information submitted."
 end
  
